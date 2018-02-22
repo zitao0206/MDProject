b54ec84b6343ed027d6e5502d20c7c6e76ed4450
Leo <zitao.li@quvideo.com>
2018-02-22 20:30:38 +0800

    TableView demo

---
+++ MDProject/MDDemos/ReactivePageKit/ReactivePageKit/XYReactBasePageTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYReactBasePageTableViewController.h
+//  MDProject
+//
+//  Created by lizitao on 2018/2/22.
+//  Copyright © 2018年 lizitao. All rights reserved.
+//
+
+#import "MDBaseViewController.h"
+
+@interface XYReactBasePageTableViewController : MDBaseViewController
+
+@end
---
+++ MDProject/MDDemos/ReactivePageKit/ReactivePageKit/XYReactBasePageTableViewController.m
@@ -0,0 +1,24 @@
+//
+//  XYReactBasePageTableViewController.m
+//  MDProject
+//
+//  Created by lizitao on 2018/2/22.
+//  Copyright © 2018年 lizitao. All rights reserved.
+//
+
+#import "XYReactBasePageTableViewController.h"
+
+@interface XYReactBasePageTableViewController ()
+
+@end
+
+@implementation XYReactBasePageTableViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+}
+
+
+@end
